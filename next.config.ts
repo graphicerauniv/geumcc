fb59b7f44cfb6111879f209095fcc7a782752935
Aman Kumar Jagdev <amankumarjagdev@gmail.com>
2025-05-14 13:17:54 +0530

    Add CORS headers for OTP API endpoints in Next.js configuration
    - Implemented custom headers for /api/send-otp and /api/verify-otp to enable CORS.
    - Configured headers to allow credentials, specify allowed origins, methods, and headers for improved API security and accessibility.

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,40 @@ const nextConfig: NextConfig = {
         ignoreBuildErrors: true,
     },
     basePath: "/mcc",
+    async headers() {
+        return [
+            {
+                source: "/api/send-otp",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,DELETE,PATCH,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+            {
+                source: "/api/verify-otp",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,DELETE,PATCH,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
